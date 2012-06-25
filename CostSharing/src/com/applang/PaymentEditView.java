393b16a63c3b59eb3c184cc5aac8e72670a38a12
sefirot <langerniklas@googlemail.com>
2012-06-25 18:35:50 +0200

    sefirot: PaymentEditVie Nr3 & Erste Transactor Anbindung

--- CostSharing/src/com/applang/PaymentEditView.java
+++ CostSharing/src/com/applang/PaymentEditView.java
@@ -8,17 +8,17 @@ import android.view.View;
 import android.widget.TableRow.LayoutParams;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioButton;
 import android.widget.TableLayout;
 import android.widget.TableRow;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.applang.db.*;
+import com.applang.shared.ShareMap;
 
 public class PaymentEditView extends Activity {
 	private static final int TYPE_NUMBER_FLAG_DECIMAL = 0;
 	private Transactor transactor;
+			ShareMap sharemap;
 	        EditText mAmountText;
 	        EditText mSubmitterText;
 	        EditText mPurposeText;
@@ -27,6 +27,7 @@ public class PaymentEditView extends Activity {
 	        EditText etLeft;
 	        EditText etRight;
 	        String firstShareString;
+	        String firstParticipantString;
 	        String shareString;
 			String amountString;
 			String nameString;
@@ -67,12 +68,8 @@ public class PaymentEditView extends Activity {
         confirmButton.setOnClickListener(new View.OnClickListener() {
         	public void onClick(View view) {
         		getEntryData();
-        		
-        		String mPaymentMessageTemplate =
-        	        	getString(R.string.payment_message_template);
-        		String message = 
-        				String.format(mPaymentMessageTemplate,submitter, amount, comment, names, shares);
-        		showToast(message);    	
+        		showToast();   
+        		saveData();
         	}
 
         });    
@@ -121,42 +118,79 @@ public class PaymentEditView extends Activity {
 		amountString = mAmountText.getText().toString();
 		amount = Double.parseDouble(amountString);
 	    submitter = mSubmitterText.getText().toString();
-	    comment = mPurposeText.getText().toString();	
+	    comment = mPurposeText.getText().toString();
 		}catch(Exception e ){
 			amount=0.0;
 			submitter = "niemand";
 			comment = "nix";
 		}
+//		if (participantNum < 4){
+	    	getEntryParticipants();		
+//	    }
 	}
 
 
 	private void getEntryParticipants() {
 		
-		try{
-			nameString = etLeft.getText().toString();
-			shareString = etRight.getText().toString();
-			share= Double.parseDouble(shareString);
-			}catch(Exception e ){
-				share=0.0;
-				nameString = "name" + participantNum;
-			}
-
-		
-		if (participantNum < 3 && participantNum > 0) {
+		if (participantNum < 2) {
 			names = new ArrayList<String>();
 			shares = new ArrayList<Double>();
+			
+			firstParticipantString = mFirstParticipantText.getText().toString();
+			
 			try{
-			firstShareString = mFirstShareText.getText().toString();
-			firstShare= Double.parseDouble(firstShareString);
+				firstShareString = mFirstShareText.getText().toString();
+				firstShare= Double.parseDouble(firstShareString);
+			}catch(Exception e ){
+				firstShare=0.0;
+			}
+			
+			names.add(firstParticipantString);
+			shares.add(firstShare);
+			
+		}else if (participantNum < 3) {
+			names = new ArrayList<String>();
+			shares = new ArrayList<Double>();
+			
+			if (mFirstParticipantText.length() > 0){
+				firstParticipantString = mFirstParticipantText.getText().toString();
+			}else {
+				firstParticipantString = "Noname1";
+			}
+			try{
+				firstShareString = mFirstShareText.getText().toString();
+				firstShare= Double.parseDouble(firstShareString);
+			}catch(Exception e ){
+				firstShare=0.0;
+			}
+			if(etLeft.length() > 0){
+				nameString = etLeft.getText().toString();
+			}else{
+				nameString = "Noname" + participantNum;
+			}
+			try{
+				shareString = etRight.getText().toString();
+				share= Double.parseDouble(shareString);
 			}catch(Exception e ){
 				share=0.0;
 			}
-			names.add(mFirstParticipantText.getText().toString());
+			names.add(firstParticipantString);
 			names.add(nameString);
 			shares.add(firstShare);
 			shares.add(share);
 			
 		} else if (participantNum > 2){
+			if(etLeft.length() > 0){
+				nameString = etLeft.getText().toString();
+			}else{
+				nameString = "Noname" + participantNum;
+			}
+			try{
+				shareString = etRight.getText().toString();
+				share= Double.parseDouble(shareString);
+			}catch(Exception e ){
+				share=0.0;
+			}
 			names.add(nameString);
 			shares.add(share);
 		} 
@@ -164,7 +198,28 @@ public class PaymentEditView extends Activity {
 		
 	}
 	
-	private void showToast(String text) {
-		Toast.makeText(this, text, Toast.LENGTH_LONG).show();	 
+	private void showToast() {
+		String mPaymentMessageTemplate =
+	        	getString(R.string.payment_message_template);
+		String message = 
+				String.format(mPaymentMessageTemplate,submitter, amount.toString(), comment, names.toString(), shares.toString());
+		
+		Toast.makeText(this, message, Toast.LENGTH_LONG).show();	 
 		}
+	
+	private void saveData() {
+		
+			if(participantNum < 2 && firstParticipantString.length() < 1){
+				transactor.performSubmission(submitter, amount, comment);
+			}else {
+				if (amount != 0.0){
+					ShareMap sharemap = new ShareMap(names.toArray(), amount, shares.toArray());
+				} else {
+					ShareMap sharemap = new ShareMap(names.toArray(), shares.toArray());
+				}
+				transactor.performExpense(submitter, comment, sharemap);
+			}
+		}
+			 
+	
 }
